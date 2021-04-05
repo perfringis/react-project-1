696d31c97487c88443e6b2a67302a0736a08e65d
Mateusz Kopko <kopko.mateusz@gmail.com>
2021-04-05 16:00:34 +0200

    make header pannel and start implementing right side bar

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -5,13 +5,13 @@ import { faLink } from '@fortawesome/free-solid-svg-icons';
 import { faCompass } from '@fortawesome/free-regular-svg-icons';
 import { faStar } from '@fortawesome/free-regular-svg-icons';
 import { faComment } from '@fortawesome/free-regular-svg-icons';
-import { faExpandAlt } from '@fortawesome/free-solid-svg-icons';
 import { faBolt } from '@fortawesome/free-solid-svg-icons';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { faChartLine } from '@fortawesome/free-solid-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faMoon } from '@fortawesome/free-regular-svg-icons';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 import image1 from '../assets/images/face1.jpeg';
 import image2 from '../assets/images/face2.jpeg';
@@ -23,7 +23,7 @@ const SideNav = () => {
       <div>
         <nav className='nav'>
           <ul>
-            <li className='logo'>
+            <li style={{ height: 102 }}>
               <a href='#'>
                 <FontAwesomeIcon icon={faLink} size='lg' />
               </a>
@@ -45,7 +45,7 @@ const SideNav = () => {
             </li>
             <li>
               <a href='#'>
-                <FontAwesomeIcon icon={faExpandAlt} size='lg' />
+                <FontAwesomeIcon icon={faChartLine} size='lg' />
               </a>
             </li>
             <li>
@@ -53,17 +53,17 @@ const SideNav = () => {
                 <FontAwesomeIcon icon={faBolt} size='lg' />
               </a>
             </li>
-            <li className='gap'></li>
-            <li className='avatar'>
+            <li style={{ height: 260 }}></li>
+            <li style={{ height: 85 }}>
               <img className='image' src={image1} alt='Logo' />
             </li>
-            <li className='avatar'>
+            <li style={{ height: 85 }}>
               <img className='image' src={image2} alt='Logo' />
             </li>
-            <li className='avatar'>
+            <li style={{ height: 85 }}>
               <img className='image' src={image3} alt='Logo' />
             </li>
-            <li className='add-user'>
+            <li style={{ height: 95 }}>
               <a href='#'>
                 <FontAwesomeIcon icon={faUserPlus} size='lg' />
               </a>
@@ -71,62 +71,74 @@ const SideNav = () => {
           </ul>
         </nav>
       </div>
-      <header className='header'>
-        <ul>
-          <li style={{ width: 95 }}></li>
-          <li>
-            <div className='circle2'></div>
-          </li>
-          <li style={{ width: 14 }}></li>
-          <li>Bank of America</li>
-          <li style={{ width: 40 }}></li>
-          <li>
-            <div className='circle'>
-              <a href='#'>
-                <FontAwesomeIcon icon={faChartLine} size='lg' />
-              </a>
-            </div>
-          </li>
-          <li style={{ width: 22 }}></li>
-          <li style={{ fontSize: 14 }}>
-            <b>+3,08%</b>
-          </li>
-          <li style={{ width: 234 }}></li>
-          <li>
-            <div>
-              <a href='#'>
-                <FontAwesomeIcon icon={faSearch} size='lg' />
-              </a>
-            </div>
-          </li>
-          <li style={{ width: 263 }}></li>
-          <li>
-            <div>
-              <a href='#'>
-                <FontAwesomeIcon icon={faMoon} size='lg' />
-              </a>
-            </div>
-          </li>
-          <li style={{ width: 43 }}></li>
-          <li>
-            <div>
-              <a href='#'>
-                <FontAwesomeIcon icon={faBell} size='lg' />
-              </a>
-            </div>
-          </li>
-          <li style={{ width: 69 }}></li>
-          <li>
-            <div className='profile-avatar'>
-              <img src={image3} alt='Logo' />
-
-              <div className='add-circle'>
-                <div className='add-circle-nested'>+</div>
+      <div className='main'>
+        <header className='header'>
+          <ul>
+            <li style={{ width: 95 }}></li>
+            <li>
+              <div className='company-logo'></div>
+            </li>
+            <li style={{ width: 14 }}></li>
+            <li>Bank of America</li>
+            <li style={{ width: 40 }}></li>
+            <li>
+              <div className='circle'>
+                <a href='#'>
+                  <FontAwesomeIcon icon={faChartLine} size='lg' />
+                </a>
               </div>
-            </div>
-          </li>
-        </ul>
-      </header>
+            </li>
+            <li style={{ width: 22 }}></li>
+            <li style={{ fontSize: 14 }}>
+              <b>+3,08%</b>
+            </li>
+            <li style={{ width: 234 }}></li>
+            <li>
+              <div>
+                <a href='#'>
+                  <FontAwesomeIcon icon={faSearch} size='lg' />
+                </a>
+              </div>
+            </li>
+            <li style={{ width: 263 }}></li>
+            <li>
+              <div>
+                <a href='#'>
+                  <FontAwesomeIcon icon={faMoon} size='lg' />
+                </a>
+              </div>
+            </li>
+            <li style={{ width: 43 }}></li>
+            <li>
+              <div>
+                <a href='#'>
+                  <FontAwesomeIcon icon={faBell} size='lg' />
+                </a>
+              </div>
+            </li>
+            <li style={{ width: 69 }}></li>
+            <li>
+              <div className='profile-avatar'>
+                <img src={image3} alt='Logo' />
+
+                <div className='add-circle'>
+                  <div className='add-circle-nested'>+</div>
+                </div>
+              </div>
+            </li>
+            <li style={{ width: 31 }}></li>
+            <li>
+              <div>
+                <a href='#'>
+                  <FontAwesomeIcon icon={faAngleDown} size='lg' />
+                </a>
+              </div>
+            </li>
+            <li style={{ width: 122 }}></li>
+          </ul>
+        </header>
+        <div className='sidenav'></div>
+      </div>
     </div>
   );
 };
